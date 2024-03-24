049c3b832708912ae9ce37a513ceb69fbfb40079
kym <melvinkimani81@gmail.com>
2024-03-24 18:00:59 +0300

    creating myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if(typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+
+}
+
+const leastFavouriteCustomer = 'John';
+
+function changeLeastFavouriteCustomer() {
+    leastFavouriteCustomer ='Jane'
+    
+}
